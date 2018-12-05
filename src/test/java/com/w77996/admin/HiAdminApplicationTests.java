427706776a994c8fa9c2e2a19e9c701c538e12d1
wuhaihui <wuhaihui@yuntutv.net>
2018-12-05 12:55:38 +0800

    代码自动生成器集成

---
+++ src/test/java/com/w77996/admin/HiAdminApplicationTests.java
@@ -0,0 +1,16 @@
+package com.w77996.admin;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest
+public class HiAdminApplicationTests {
+
+    @Test
+    public void contextLoads() {
+    }
+
+}
